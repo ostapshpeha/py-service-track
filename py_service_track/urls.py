dafa79efe62107d266198041c2192f73a3ec5fd1
Ostap Shp <stark171705@gmail.com>
2025-12-18 16:40:56 +0200

    App Notes Development | New feature - successfully integrated pictures in django admin panel for Note model. All pictures will be saved in media/ dir

--- py_service_track/urls.py
+++ py_service_track/urls.py
@@ -28,3 +28,9 @@ urlpatterns = [
         path("", include("orders.urls")),
         path("", include("notes.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
