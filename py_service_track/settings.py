8666cde255387743deb0835a3166a9bcc8bc6d37
Ostap Shp <stark171705@gmail.com>
2025-12-19 14:12:34 +0200

    Project | Copied static files from free template for better customisation + init files for query_transform

--- py_service_track/settings.py
+++ py_service_track/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
