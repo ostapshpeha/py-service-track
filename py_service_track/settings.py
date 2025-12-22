18ae95e6571257896f6099feb08e07373fd1b408
Ostap Shp <stark171705@gmail.com>
2025-12-22 20:09:50 +0200

    Changed settings, moved debug and secret key to .env

--- py_service_track/settings.py
+++ py_service_track/settings.py
@@ -14,20 +14,26 @@ from pathlib import Path
 
 import environ
 
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$5^_b_@3xpxy1&sty5l$#ke42)qzau$ktocu!8$u97q)7)9(mc'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = ["*"]
+
+DEBUG = env("DEBUG")
+
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
