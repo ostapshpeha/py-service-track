14845865355b0cef6ca09c55205c110cbbb61fc9
Ostap Shp <stark171705@gmail.com>
2025-12-23 14:32:01 +0200

    Refactored Settings, Added settings for development and production | Added PostgreSQL for production

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for py_service_track project.
+WSGI config for config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py_service_track.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
